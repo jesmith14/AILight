bb174c38fe3b81ca60f85606bba76501d29301f3
landongerrits <landon.gerrits@me.com>
2018-10-30 13:05:27 -0700

    other settings is now functional

---
+++ QuickStartApp_iOS/HueQuickStartApp-iOS/HueQuickStartApp-iOS-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "PHControlLightsViewController.h"
+#import "PHAppDelegate.h"
+
+#import <HueSDK_iOS/HueSDK.h>
+#define MAX_HUE 65535
+
