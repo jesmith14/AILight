dad6759b6eb6deb34e7c7c4a3b14f845a33da181
Kalyan Vejalla <kalyan.vejalla392@gmail.com>
2018-12-05 20:19:03 -0800

    Added backprop function to flask file -- kalyan

--- swift_to_flask.py
+++ swift_to_flask.py
@@ -52,6 +52,83 @@ def ImageUpload():
         #return jsonify(color1=color1,color2=color2,color3=color3)
         return jsonify(values=returnVals)
     return jsonify(error='Request Failed')
+
+@app.route('/backPropInit',methods=['POST'])
+def backPropInit():
+
+    NN = torch.load("NN")
+
+    data = request.data
+    inputList = data[0]
+    outputList = data[1]
+    userList = data[2]
+    callBackProp(inputList, outputList, userList, NN)
+
+    return jsonify(resp= "Backpropogation Neural Net Started Success")
+
+
+def callBackProp(inputList, outputList, userList, NeuralNet):
+
+    #inputList = (r1, g1, b1, w1, r2, g2, b2, w2, r3, g, b3, w3) -- input colors from k-means
+    #ouputList = (h1, s1, b1, h2, s2, b2, h3, s3, b3) -- list of colors that program selects
+
+    #userList = (h1, s1, b1, h2, s2, b2, h3, s3, b3) -- list of colors that user chooses
+
+    # INPUTS - X
+
+    r1 = inputList[0]
+    g1 = inputList[1]
+    b1 = inputList[2]
+    w1 = inputList[3]
+
+    r2 = inputList[4]
+    g2 = inputList[5]
+    b2 = inputList[6]
+    w2 = inputList[7]
+
+    r3 = inputList[8]
+    g3 = inputList[9]
+    b3 = inputList[10]
+    w3 = inputList[11]
+
+    # OUTPUTS - y
+
+    h1 = outputList[0]
+    s1 = outputList[1]
+    b1 = outputList[2]
+
+    h2 = outputList[3]
+    s2 = outputList[4]
+    b2 = outputList[5]
+
+    h3 = outputList[6]
+    s3 = outputList[7]
+    b3 = outputList[8]
+
+    # CORRECT VALUES CHOSEN BY USER
+
+    uh1 = userList[0]
+    us1 = userList[1]
+    ub1 = userList[2]
+
+    uh2 = userList[3]
+    us2 = userList[4]
+    ub2 = userList[5]
+
+    uh3 = userList[6]
+    us3 = userList[7]
+    ub3 = userList[8]
+
+
+    X = torch.tensor(([r1], [g1], [b1], [w1], [r2], [g2], [b2], [w2], [r3], [g3], [b3], [w3]), dtype=torch.float)
+    y = torch.tensor(([h1], [s1], [b1], [h2], [s2], [b2], [h3], [s3], [b3]), dtype=torch.float)
+
+    o = torch.tensor(([uh1], [us1], [ub1], [uh2], [us2], [ub2], [uh3], [us3], [ub3]), dtype=torch.float)
+
+    NeuralNet.backward(X, y, o)
+
+    NeuralNet.saveWeights(NeuralNet)
+
 def getArray(weights):
     array = []
     for item in weights:
@@ -61,6 +138,7 @@ def getArray(weights):
         array.append(item[0])
     #print("Formatted list for Neural Net: " + str(array));
     return array
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
